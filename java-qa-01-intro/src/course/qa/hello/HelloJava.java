8bc73dfe0469ee26470e0b3aac6fbd478098bc0e
i.vachev <i.vachev@is-bg.net>
2022-04-18 20:03:24 +0300

    error added on purpose

--- java-qa-01-intro/src/course/qa/hello/HelloJava.java
+++ java-qa-01-intro/src/course/qa/hello/HelloJava.java
@@ -8,5 +8,6 @@ public class HelloJava {
         System.out.println("Enter your name:");
         String name = sc.nextLine();
         System.out.println("Hello " + name + ", from java 11!");
+        this is not java
     }
 }
